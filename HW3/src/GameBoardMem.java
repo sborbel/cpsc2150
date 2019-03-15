c7f12f47f56cf9882ae06bbe5999b531f1e0af2a
Sean Borbely <sborbel@clemson.edu>
2019-03-14 23:33:59 -0400

    Update GameBoardMem.java

--- HW3/src/GameBoardMem.java
+++ HW3/src/GameBoardMem.java
@@ -5,6 +5,7 @@
  * GameBoardMem: Uses a Map to represent a board of player tokens. Memory-efficient.
  *
  * Invariant: board will update after each move to represent the spaces already taken up
+ *            Size of the board does not change
  * Correspondence: r: rows, c: cols
  */
 import java.util.*;
@@ -16,7 +17,15 @@ public class GameBoardMem extends AbsGameBoard implements IGameBoard {
     private int posToWin;
     private int numTotalMoves;
 
+    /**
+     Parameterized constructor for GameBoardMem
+     @Requires ("0 < boardRows < 10 && 0 < boardCols < 10" && inARow > 0)
+     @Ensures each space in board is filled with a blank space
+     @Param boardRows is the number of rows for the board
+     @Param boardCols is the number of columns in the board
+     @Param is the number of positions a player must get in a row to win
 
+     */
     public GameBoardMem(int boardRows, int boardCols, int inARow){
         rows = boardRows;
         cols = boardCols;
@@ -35,7 +44,6 @@ public class GameBoardMem extends AbsGameBoard implements IGameBoard {
             board.put(c, new LinkedList<>());
             board.get(c).add(p);
         }
-
     }
 
     @Override
